f09f1a037f649535edcedcd6bcf00c7aa0f43ecf
Nikita Peplov <npeplov@yandex.ru>
2020-06-01 18:45:27 +0300

    Using dict to collect word: counter

--- 3.1file_formats.py
+++ 3.1file_formats.py
@@ -1,34 +1,6 @@
 # выводить топ 10 самых часто встречающихся в новостях слов
 # длиннее 6 символов для каждого файла.
-""""
-1. Открыть файл newsafr.json
-2. Парсером преобразовать строки в словарь
-3. Нужен items.description
-4. Найти одинаковые слова в списке
 
-   сделать список из списков:
-   слово: количество повторений
-
-   for i, word in enumerate(words):
-     words[0], word = 'Africa'
-     count = words.count(word) // 6
-     words[i].pop() // last = 'Africa'
-     удалить все элементы Africa из words:
-     for ind, del_word in enumerate(words):
-       if word in words:
-         words.pop(ind)
-
-       my_list.append()
-     my_list.append([word, words.count(word)])
-
-   count нельзя потому что можно сравнить только со списком из 2 элементов
-   нужно использовать pop
-
-   list.count(x)
-
-   dict.get(key[, default]) -
-   возвращает значение ключа, но если его нет возвращает default (по умолчанию None).
-"""
 import json
 import xml.etree.ElementTree as ET
 
@@ -63,6 +35,7 @@ def select_descriptions(items_list):
     return descriptions_list
 
 
+# список всех слов с дублями, одинаковые идут друг за другом
 def select_words(descriptions_list):
     words_list = []
     for i, val in enumerate(descriptions_list):
@@ -73,31 +46,20 @@ def select_words(descriptions_list):
     return sorted(words_list)
 
 
+# на входе список слов с дублями words_list
 def counter_of_words(words_list):
-    count_words = []
-    for i, word in enumerate(words_list):
+    # одинаковые идут друг за другом: [advance, advance, africa, africa...]
+    # создаем словарь
+    count_words_dict = {}
+    # идем по элементам списка
+    for word in words_list:
+        # считаем количество повторений:
         count = words_list.count(word)
-        count_words.append([count, word])
-    i = 0
-    count_words.sort(reverse=True)
-    return count_words
-
-
-def del_doubles(count_words):
-    i = 0
-    while i < len(count_words) - 1:
-        # my_list[0] = [42, 'Африки'] = my_list[1]
-        if count_words[i] == count_words[i+1]:
-            # удаляем элемент my_list[0]
-            count_words.pop(i)
-            # возврат счетчика элементов к индексу 0
-            # первого встреченного значения [42, 'Африки']
-            i = count_words.index(count_words[i])
-        else:
-            # если текущий элемент 0 не равен следующему 1 [42, 'Африки']
-            # переходим к след. элементу 1 [40, 'туристов']
-            i += 1
-    return count_words
+        # словарь {слово: число}, одинаковые ключи не добавятся
+        count_words_dict[word] = count
+    # получили словарь {'advance': 2} и тд
+    # сортируем по значениям item[1]
+    return sorted(count_words_dict.items(), key=lambda item: item[1], reverse=True)
 
 
 def top_ten(input_list):
@@ -111,8 +73,6 @@ def top_ten(input_list):
 
 if __name__ == '__main__':
     print('JSON')
-    json_list = del_doubles(counter_of_words(select_words(select_descriptions(select_items(open_json_file('newsafr.json'))))))
-    top_ten(json_list)
+    top_ten(counter_of_words(select_words(select_descriptions(select_items(open_json_file('newsafr.json'))))))
     print('XML')
-    xml_list = del_doubles(counter_of_words(select_words(open_xml_file('newsafr.xml'))))
-    top_ten(xml_list)
+    top_ten(counter_of_words(select_words(open_xml_file('newsafr.xml'))))
